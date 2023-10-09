89106784fd78cbb10845736a1e1eafae147790b8
Rafa Canosa <rafacv23@gmail.com>
2023-10-09 18:21:43 +0200

    added total amount for every category item

--- src/components/category-btn.jsx
+++ src/components/category-btn.jsx
@@ -12,7 +12,11 @@ const CategoryBtn = ({ datos, filterByCategory, setFilteredData, filteredData, c
     <nav className={show ? "nav-active" : "nav"}>
         <button className="category-btn" onClick={() => handleShow()}>Filtros</button>
         {show ? categories.map((dato) => (
+          <div key={dato.id} className="category-item-mobile" onClick={() => filterByCategory(dato.id)}>
+                <img className="category-item-img" src={dato.category_img} alt={dato.category_name}/>
                 <button className="category-mobile" key={dato.id} onClick={() => filterByCategory(dato.id)}>{dato.category_name}</button>
+                <p className="category-total-products">{dato.total_products}</p>
+              </div>
         )) : null}
     </nav>
   )
--- src/components/category-container.jsx
+++ src/components/category-container.jsx
@@ -13,6 +13,7 @@ const CategoryContainer = ({ datos, filterByCategory, setFilteredData, filteredD
               <div key={category.id} className="category-item" onClick={() => filterByCategory(category.id)}>
                 <img className="category-item-img" src={category.category_img} alt={category.category_name}/>
                 <button className="category-item-btn" key={category.id} onClick={() => filterByCategory(category.id)}>{category.category_name}</button>
+                <p className="category-total-products">{category.total_products}</p>
               </div>
             ))}
         </ul>
